aa4e55b12b3788cd896e02584f9c771cfbab9892
sandibi13 <sandipanb223@gmail.com>
2024-06-13 16:38:18 +0530

    added and configured tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,24 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Codetribe" },
+    {
+      name: "description",
+      content:
+        "Join CodeTribe, the ultimate social networking platform for developers to connect, collaborate, and share their coding journey.",
+    },
+  ];
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex items-center justify-center h-full w-full">
+      <h1 className="text-3xl font-bold text-zinc-600">Codetribe</h1>
     </div>
   );
 }
