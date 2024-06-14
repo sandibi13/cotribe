69a9d618cf7f0ad9bcb07cfca3df6c87a54ad23d
sandibi13 <sandipanb223@gmail.com>
2024-06-14 11:46:31 +0530

    added shadcn.ui

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{ fontFamily: "inter, sans-serif" }}>
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
-export default function Index() {
+export default function IndexPage() {
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center w-screen h-screen">
       <h1 className="text-3xl font-bold text-zinc-600">Codetribe</h1>
     </div>
   );
