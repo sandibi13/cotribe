eee2fcaeb59491b6d525dd9ed8a7db60930b9a5f
sandibi13 <sandipanb223@gmail.com>
2024-06-14 21:31:57 +0530

    added globe and particles components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import { Link } from "@remix-run/react";
+import Globe from "~/components/globe";
+import Particles from "~/components/particles";
 import { Button } from "~/components/ui/button";
 
 export default function IndexPage() {
@@ -7,6 +9,8 @@ export default function IndexPage() {
       <nav />
 
       <div className="container md:flex justify-center items-center px-4 md:px-6 flex-1">
+        <Particles className="absolute inset-0 -z-10" />
+
         <div className="flex flex-col items-center space-y-4 text-center p-4 md:w-1/2">
           <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
             A{" "}
@@ -20,9 +24,18 @@ export default function IndexPage() {
             to Connect
           </h1>
 
+          <p className="text-muted-foreground mt-2">
+            Join a vibrant community of developers. Share ideas, collaborate on
+            projects, and grow together.
+          </p>
+
           <Button asChild>
             <Link to="/login">Join our Community</Link>
           </Button>
+        </div>
+
+        <div className="relative md:w-1/2">
+          <Globe />
         </div>
       </div>
     </section>
