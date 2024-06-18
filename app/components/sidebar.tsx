13d8a200abca8c0de3331cbd6c7b75dd6f88b841
sandibi13 <sandipanb223@gmail.com>
2024-06-18 21:01:45 +0530

    fix: sidebar scaling issue

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -17,14 +17,14 @@ export const Sidebar = () => {
 
   return (
     <div className="hidden sm:block">
-      <div className="fixed top-4 left-4 bottom-4 w-20 md:w-64 rounded-lg shadow-lg border bg-muted/40 p-4 flex flex-col">
+      <div className="fixed top-4 left-4 bottom-4 w-20 lg:w-64 rounded-lg shadow-lg border bg-muted/40 p-4 flex flex-col">
         <div className="flex h-14 items-center border-b pb-2 mb-2">
           <Link
             to="/"
-            className="flex items-center justify-center md:justify-start gap-2 font-semibold p-2"
+            className="flex items-center justify-center lg:justify-start gap-2 font-semibold p-2"
           >
             <Logo className="h-8 w-8" />
-            <span className="hidden md:inline">Codetribe</span>
+            <span className="hidden lg:inline">Codetribe</span>
           </Link>
         </div>
         <div className="flex-1 overflow-auto">
@@ -33,14 +33,14 @@ export const Sidebar = () => {
               <Link
                 key={label}
                 to={to}
-                className={`flex items-center justify-center md:justify-start gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
+                className={`flex items-center justify-center lg:justify-start gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
                   location.pathname === to
                     ? "bg-muted text-primary"
                     : "text-muted-foreground"
                 }`}
               >
                 <Icon className="h-5 w-5" />
-                <span className="hidden md:inline">{label}</span>
+                <span className="hidden lg:inline">{label}</span>
               </Link>
             ))}
           </nav>
