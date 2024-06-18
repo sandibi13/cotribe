53876294c9ad935957c6420d5fdceed3614b1c12
sandibi13 <sandipanb223@gmail.com>
2024-06-18 15:21:04 +0530

    added tablet screen support in sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -16,11 +16,14 @@ export const Sidebar = () => {
   const location = useLocation();
 
   return (
-    <div className="hidden md:block">
-      <div className="fixed top-4 left-4 bottom-4 w-64 rounded-lg shadow-lg border bg-muted/40 p-4 flex flex-col">
+    <div className="hidden sm:block">
+      <div className="fixed top-4 left-4 bottom-4 w-20 md:w-64 rounded-lg shadow-lg border bg-muted/40 p-4 flex flex-col">
         <div className="flex h-14 items-center border-b pb-2 mb-2">
-          <Link to="/" className="flex items-center gap-2 font-semibold">
-            <Logo className="h-6 w-6" />
+          <Link
+            to="/"
+            className="flex items-center justify-center md:justify-start gap-2 font-semibold p-2"
+          >
+            <Logo className="h-8 w-8" />
             <span className="hidden md:inline">Codetribe</span>
           </Link>
         </div>
@@ -30,20 +33,20 @@ export const Sidebar = () => {
               <Link
                 key={label}
                 to={to}
-                className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
+                className={`flex items-center justify-center md:justify-start gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
                   location.pathname === to
                     ? "bg-muted text-primary"
                     : "text-muted-foreground"
                 }`}
               >
-                <Icon className="h-4 w-4" />
+                <Icon className="h-5 w-5" />
                 <span className="hidden md:inline">{label}</span>
               </Link>
             ))}
           </nav>
         </div>
-        <div className="p-4">
-          <UserButton showName />
+        <div className="p-2">
+          <UserButton />
         </div>
       </div>
     </div>
--- app/routes/_site.tsx
+++ app/routes/_site.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "~/components/sidebar";
 
 export default function SiteLayout() {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full grid-cols-[80px_1fr] md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
       <Outlet />
     </div>
