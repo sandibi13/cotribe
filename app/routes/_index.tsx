dffebd9b423db02e4f4ebdd9fa1f72cb1f9a2f7f
sandibi13 <sandipanb223@gmail.com>
2024-07-17 14:25:34 +0530

    initialized with t3-stack

--- app/routes/_index.tsx
+++
@@ -1,7 +0,0 @@
-export default function IndexPage() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Cotribe</h1>
-    </div>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
-import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/react";
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 } satisfies Config;
